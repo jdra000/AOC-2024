02e2b8091edf6499a640f3cc097c109d24fb37f7
jdra000 <juandavid.rard@gmail.com>
2026-02-08 00:29:57 -0500

    day3 two parts

--- day3/day3.go
+++ day3/day3.go
@@ -3,46 +3,76 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"log"
-	"os"
-	"regexp"
+    "regexp"
+    "strings"
 
 	"github.com/jdra000/AOC-2024/helper"
 )
 
-func toInt(str []string) []int {
-	var nums []int
+func toInt(str []string) (nums []int) {
 	for _, v := range str {
 		nums = append(nums, helper.MustAtoi(v))
 	}
 	return nums
 }
 
-func performCalc(file io.Reader) {
+func calc(str string, re *regexp.Regexp) (count int) {
+    number_regex := regexp.MustCompile(`\d+`)
+    validMuls := re.FindAllString(str, -1)
+
+    for _, mul := range validMuls {
+        validNumbers := toInt(number_regex.FindAllString(mul, -1))
+        count += validNumbers[0] * validNumbers[1]
+    }
+
+    return count
+}
+
+func partOne(f string, re *regexp.Regexp) (count int) {
+
+    file := helper.OpenFile(f)
+    defer file.Close()
+
 	scanner := bufio.NewScanner(file)
-	re := regexp.MustCompile(`mul\(\d+,\d+\)`)
-	var count int
-	var validStrings []string
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		//fmt.Println(line)
-		validStrings = re.FindAllString(line, -1)
-
-		for _, v := range validStrings {
-			re2 := regexp.MustCompile(`\d+`)
-			validNumbers := toInt(re2.FindAllString(v, -1))
-			count += validNumbers[0] * validNumbers[1]
-		}
+        count += calc(line, re)
 	}
-	fmt.Println(count)
+	return count
+}
+
+func partTwo(f string, re *regexp.Regexp) (count int) {
+
+    file := helper.OpenFile(f)
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    do_regex := regexp.MustCompile(`do\(\)`)
+
+    for scanner.Scan(){
+        line := scanner.Text()
+
+        split_str := strings.Split(line, "don't()")
+
+        for i, str := range split_str {
+            if i == 0 {
+                count += calc(str, re)
+		    }
+
+            if strings.Contains(str, "do()"){
+                index := do_regex.FindStringIndex(str)
+                count += calc(str[index[1]:], re)
+		    }
+        }
+    }
+    return count
 }
 
 func main() {
-	file, err := os.Open("./file.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	performCalc(file)
+    f := "./file.txt"
+	regex := regexp.MustCompile(`mul\(\d+,\d+\)`)
+
+	fmt.Printf("results day1: %d\n", partOne(f, regex))
+	fmt.Printf("results day2: %d\n", partTwo(f, regex))
 }
--- helper/helper.go
+++ helper/helper.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"log"
 	"strconv"
+    "os"
 )
 
 func Abs(x int) int {
@@ -18,4 +19,12 @@ func MustAtoi(s string) int {
 		log.Fatal(err)
 	}
 	return num
+}
+
+func OpenFile(s string) (f *os.File){
+   f, err := os.Open(s)
+   if err != nil {
+        log.Fatal(err)
+   }
+   return f
 }
